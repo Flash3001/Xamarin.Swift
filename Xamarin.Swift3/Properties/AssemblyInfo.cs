fe9376723a13215192ad61a404fbb5ee761e11eb
Lucas <lucas@yoursoft.com.br>
2016-11-05 20:49:30 -0200

    support for Xcode 8.1

--- Xamarin.Swift3/Properties/AssemblyInfo.cs
+++ Xamarin.Swift3/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Xamarin.Swift23.Support")]
+[assembly: AssemblyTitle("Xamarin.Swift3.Support")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Flash3001")]
