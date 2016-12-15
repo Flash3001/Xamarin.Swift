d9808c686e09887a65cb9abf6208f8670eab09d9
Lucas Teixeira <flash3001@msn.com>
2016-12-15 17:10:26 -0200

    moving to 3.0.2

---
+++ copy_new_dylibs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+DEVELOPER_DIR=`xcode-select --print-path`
+if [ ! -d "${DEVELOPER_DIR}" ]; then
+	echo "No developer directory found!"
+	exit 1
+fi
+
+for PROJECT in $(ls -1 -d Xamarin.Swift3.*/); do
+    for SWIFT_LIB in $(ls -1 "${PROJECT}/Frameworks/"); do
+        echo "Copying ${SWIFT_LIB}"
+        cp "${DEVELOPER_DIR}/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/iphoneos/${SWIFT_LIB}" "${PROJECT}/Frameworks"
+        cp "${DEVELOPER_DIR}/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/iphonesimulator/${SWIFT_LIB}" "${PROJECT}/SwiftFrameworksSimulator"
+    done
+done
