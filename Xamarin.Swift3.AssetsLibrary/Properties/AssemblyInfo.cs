abee0b946c1a8bb9746baa1b83d15e4a9a39bcc6
Lucas Teixeira <flash3001@msn.com>
2016-12-15 17:22:02 -0200

    moving to 3.0.2

--- Xamarin.Swift3.AssetsLibrary/Properties/AssemblyInfo.cs
+++ Xamarin.Swift3.AssetsLibrary/Properties/AssemblyInfo.cs
@@ -11,13 +11,13 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("")]	
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("3.0.1")]
+[assembly: AssemblyVersion("3.0.2")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
